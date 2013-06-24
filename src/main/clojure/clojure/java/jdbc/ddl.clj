563c9a2c4d8ed03f8335df06435a0561a56b242d
Matt Oquist <moquist@majen.net>
2013-06-23 21:48:57 -0400

    Add fns ddl/create-index and ddl/drop-index.
    Signed-off-by: Sean Corfield <sean@corfield.org>

--- src/main/clojure/clojure/java/jdbc/ddl.clj
+++ src/main/clojure/clojure/java/jdbc/ddl.clj
@@ -48,3 +48,31 @@ not very sophisticated." }
   "Given a table name, return the DDL string for dropping that table."
   [name]
   (format "DROP TABLE %s" (sql/as-str identity name)))
+
+(defn create-index
+  "Given an index name, table name, vector of column names, and
+  (optional) is-unique, return the DDL string for creating an index.
+
+   Examples:
+   (create-index :indexname :tablename [:field1 :field2] :unique)
+   \"CREATE UNIQUE INDEX indexname ON tablename (field1, field2)\"
+
+   (create-index :indexname :tablename [:field1 :field2])
+   \"CREATE  INDEX indexname ON tablename (field1, field2)\""
+  [index-name table-name cols & is-unique]
+  (let [cols-string (apply str
+                           (interpose ", "
+                                      (map (sql/as-str identity)
+                                           cols)))
+        is-unique (if is-unique "UNIQUE" "")]
+    (format "CREATE %s INDEX %s ON %s (%s)"
+            is-unique
+            (sql/as-str identity index-name)
+            (sql/as-str identity table-name)
+            cols-string)))
+
+(defn drop-index
+  "Given an index name, return the DDL string for dropping that index."
+  [name]
+  (format "DROP INDEX %s" (sql/as-str identity name)))
+
