6282f32b21a6727a31553345326a0aa3c87168ac
Sean Corfield <sean@corfield.org>
2015-07-10 09:13:51 -0700

    Clarified pom.xml/project.clj roles
    Added comments to project.clj clarifying that pom.xml is the "system of
    record".
    
    Also took the opportunity to add 1.8 snapshot and bump 1.7 to stable
    version for multi-version testing.

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
-(defproject org.clojure/java.jdbc "0.3.7-SNAPSHOT"
+;; NOTE: This project.clj file exists purely to make it easier to
+;; develop and test java.jdbc locally. The pom.xml file is the
+;; "system of record" as far as the project version is concerned.
+
+(defproject org.clojure/java.jdbc "0.3.8-SNAPSHOT"
   :description "A low-level Clojure wrapper for JDBC-based access to databases."
   :parent [org.clojure/pom.contrib "0.1.2"]
   :url "https://github.com/clojure/java.jdbc"
@@ -7,7 +11,12 @@
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
-  :dependencies [[org.clojure/clojure "1.7.0-beta3"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 ;; These are just the versions most recently test against
+                 ;; for your own projects, use whatever version is most
+                 ;; appropriate for you. Again, note that this project.clj
+                 ;; file exists for convenience -- the pom.xml file is the
+                 ;; "system of record" as far as dependencies go!
                  [org.apache.derby/derby "10.11.1.1"]
                  [hsqldb "1.8.0.10"]
                  [net.sourceforge.jtds/jtds "1.3.1"]
@@ -15,15 +24,16 @@
                  [org.postgresql/postgresql "9.4-1201-jdbc41"]
                  [org.xerial/sqlite-jdbc "3.8.10.1"]
                  ;; if you have the MS driver in your local repo
-                 ;; [sqljdbc4 "4.0"]
+                 [sqljdbc4 "4.0"]
                  ]
   :profiles {:1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0-master-SNAPSHOT"]]}
              }
-  :aliases {"test-all" ["with-profile" "test,1.2:test,1.3:test,1.4:test,1.5:test,1.6:test,1.7" "test"]
-            "check-all" ["with-profile" "1.2:1.3:1.4:1.5:1.6:1.7" "check"]}
+  :aliases {"test-all" ["with-profile" "test,1.2:test,1.3:test,1.4:test,1.5:test,1.6:test,1.7:test,1.8" "test"]
+            "check-all" ["with-profile" "1.2:1.3:1.4:1.5:1.6:1.7:1.8" "check"]}
   :min-lein-version "2.0.0")
