2c5274b7758fba7de366541fdcadeabe38a61f2d
Sean Corfield <sean@corfield.org>
2017-06-28 23:13:56 -0700

    Drop 1.4 locally; bump 1.9 to Alpha 17

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  ;; These are just the versions most recently test against
                  ;; for your own projects, use whatever version is most
                  ;; appropriate for you. Again, note that this project.clj
@@ -30,8 +30,7 @@
                  ;; if you have the MS driver in your local repo
                  [sqljdbc4 "4.0"]]
 
-  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+  :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
@@ -41,6 +40,6 @@
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"
                  "ws-archiva" "https://d259tvauhnips9.cloudfront.net/archiva/repository/internal/"}
   ;; include dev profile with 1.9 to pull in test.check
-  :aliases {"test-all" ["with-profile" "test,1.4:test,1.5:test,1.6:test,1.7:test,1.8:dev,test,1.9" "test"]
-            "check-all" ["with-profile" "1.4:1.5:1.6:1.7:1.8:1.9" "check"]}
+  :aliases {"test-all" ["with-profile" "test,1.5:test,1.6:test,1.7:test,1.8:dev,test,1.9" "test"]
+            "check-all" ["with-profile" "1.5:1.6:1.7:1.8:1.9" "check"]}
   :min-lein-version "2.0.0")
