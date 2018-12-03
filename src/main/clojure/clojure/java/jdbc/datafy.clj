3e0e693d620c98aa80eda417a69d405e65cf808e
Sean Corfield <sean@corfield.org>
2018-12-02 17:59:31 -0800

    Update copyright

--- src/main/clojure/clojure/java/jdbc/datafy.clj
+++ src/main/clojure/clojure/java/jdbc/datafy.clj
@@ -1,4 +1,4 @@
-;;  Copyright (c) 2008-2018 Sean Corfield, Stephen C. Gilardi. All rights reserved.
+;;  Copyright (c) 2018 Sean Corfield. All rights reserved.
 ;;  The use and distribution terms for this software are covered by
 ;;  the Eclipse Public License 1.0
 ;;  (http://opensource.org/licenses/eclipse-1.0.php) which can be
